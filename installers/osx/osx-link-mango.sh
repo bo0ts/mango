90caf5bba6f9e1729ff95688d71c93c903ab54dc
Amos Joshua <amos@amos-joshuas-macbook-pro.local>
2011-03-15 15:33:19 -0700

    Added installers folder and osx installer

---
+++ installers/osx/osx-link-mango.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+ln -s /Library/Frameworks/Mango.framework/Versions/1.0 /Library/Frameworks/Mango.framework/Versions/Current
+ln -s /Library/Frameworks/Mango.framework/Versions/Current/Headers /Library/Frameworks/Mango.framework/Headers
+ln -s /Library/Frameworks/Mango.framework/Versions/Current/lib     /Library/Frameworks/mango.framework/lib
+ln -s /Library/Frameworks/Mango.framework/Versions/Current/bin/mango /usr/bin/mango
+ln -s /Library/Frameworks/Mango.framework/Versions/Current/doc /Library/Frameworks/Mango.framework/doc
+ln -s /Library/Frameworks/Mango.framework/Versions/Current/Mango /Library/Frameworks/Mango.framework/Mango
