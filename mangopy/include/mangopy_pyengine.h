9522440fda1cb437d7e5f8036cfb30d2d6003bdf
Amos Joshua <amos@Macintosh.local>
2011-06-29 00:25:25 +0300

    added license information to headers

--- mangopy/include/mangopy_pyengine.h
+++ mangopy/include/mangopy_pyengine.h
@@ -1,3 +1,24 @@
+/*
+ Copyright (c) 2011 Amos Joshua
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
 #include "mangopy_python.h"
 #include <vector>
 #include "mango.h"
@@ -9,84 +30,76 @@
 
 #include "mangopy_core.h"
 
-typedef std::pair<Mango::Core::Object *, PyObject *> ObjectPythonEventRecord;
+namespace MangoPy{
 
-
-
-class PythonScriptError : public Mango::Core::Error{
-    public:
-      PythonScriptError(const char *origObjectType, const char *originatingMethodName, const char *m=""): Error(origObjectType, originatingMethodName, m){
-      }
-
-      virtual const char *objectType() const{
-          return "PythonScriptError";
-      }
-      //virtual void trace() const throw();
-};
-
-
-
-
-class PyEngine : public Mango::Core::CoreEngine, public Mango::Core::ObjectContainer{
-    public:
-        PyEngine();
-        ~PyEngine();
-
-        virtual const char *objectType() const{
-            return "PyEngine";
-        }
-
-        const char *getObjectEventMethodName(int event_type);
-
-        virtual bool setScriptedEvent(mpy_Object* object, int event_mask);
-    	virtual bool removeScriptedEvent(mpy_Object* object, int event_mask);
-        virtual bool toggleScriptedEvent(mpy_Object* object, int event_mask);
-        virtual bool objectHasScriptedEvent(mpy_Object *object, int event_mask);        
-
-        virtual void removeObject(Mango::Core::Object* object_to_remove, int BOCT_ID);
-        
-        /*
-        virtual bool setEvent(Object* object, int event_type);
-        virtual bool removeEvent(Object* object, int event_type);
-        */
-        
-        virtual void executeCallback(mpy_Object *obj, int event_type);
-
-
-        // Debugging
-
-        void __printPythonEvents(int event_type){
-            int size = python_events[event_type].size();
-            std::cout << std::endl << "----------------------------------------------------------------" << std::endl;
-		    std::cout << "python_events[" << event_type << "] with " << size << "events " << std::endl;
-		    for (int i = 0; i < size; i += 1){
-		        std::cout << "  " << i << ") python_events[" << event_type << "][ " << i << "] = " << &(python_events[event_type][i]) << std::endl;
-		        std::cout << "  " << i << ") python_events[" << event_type << "][ " << i << "].first = " << python_events[event_type][i].first << std::endl;
-		        std::cout << "  " << i << ") python_events[" << event_type << "][ " << i << "].second = " << python_events[event_type][i].second << std::endl;
-		    }     
-		    std::cout << std::endl << "----------------------------------------------------------------" << std::endl;
-        }
-
-    protected:
-
-        virtual void evt_render();
-        virtual void evt_draw();
-        //virtual void evt_pre_step();
-        virtual void evt_step();
-        //virtual void evt_post_step();
-        virtual void evt_input();
-
-        std::vector<ObjectPythonEventRecord> python_events[ENGINE_MAX_EVENT_TYPES];
-        static int BOC_TYPE_IDS[ENGINE_MAX_EVENT_TYPES];
-};
-
-
-
-
-
-
-
-
-
+  //// @cons
+  typedef std::pair<Mango::Core::Object *, PyObject *> ObjectPythonEventRecord;
+  //// @endcond
+    
+  /**
+   * An error representing an exception in a Python script
+   */ 
+  class PythonScriptError : public Mango::Core::Error{
+  public:
+  PythonScriptError(const char *origObjectType, const char *originatingMethodName, const char *m=""): Error(origObjectType, originatingMethodName, m){
+    }
+    
+    //// @cond
+    virtual const char *objectType() const{
+      return "PythonScriptError";
+    }
+    //// @endcond
+  };
+  
+  
+  /**
+   * An engine object derived from Core::CoreEngine that adds support
+   * for triggerring Python methods as callback functions. A number of
+   * methods dealing with such events ("scripted events") are added. In
+   * simulations written in Python, the global Engine is an instance of
+   * this class
+   */
+  class PyEngine : public Mango::Core::CoreEngine, public Mango::Core::ObjectContainer{
+      public:
+          PyEngine();
+          ~PyEngine();
+  
+	  //// @cond
+          virtual const char *objectType() const{
+              return "PyEngine";
+          }
+  
+          const char *getObjectEventMethodName(int event_type);
+	  //// @endcond
+	  
+          virtual bool setScriptedEvent(mpy_Object* object, int event_mask);
+      	virtual bool removeScriptedEvent(mpy_Object* object, int event_mask);
+          virtual bool toggleScriptedEvent(mpy_Object* object, int event_mask);
+          virtual bool objectHasScriptedEvent(mpy_Object *object, int event_mask);        
+  
+          virtual void removeObject(Mango::Core::Object* object_to_remove, int BOCT_ID);
+          
+          /*
+          virtual bool setEvent(Object* object, int event_type);
+          virtual bool removeEvent(Object* object, int event_type);
+          */
+          
+          virtual void executeCallback(mpy_Object *obj, int event_type);
+  
+  
+      protected:
+  
+          virtual void evt_render();
+          virtual void evt_draw();
+          virtual void evt_step();
+          virtual void evt_input();
+  
+          std::vector<ObjectPythonEventRecord> python_events[ENGINE_MAX_EVENT_TYPES];
+          static int BOC_TYPE_IDS[ENGINE_MAX_EVENT_TYPES];
+  };
+  
+  
+ 
+} // MangoPy  
 
 #endif // MANGOPY_PYENGINE
