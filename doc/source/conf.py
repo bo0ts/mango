38316f7649b7da9ec5e012efe03660b608a47802
Amos Joshua <amos@amos-laptop.home>
2012-02-26 10:47:10 +0100

    copyrights

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Mango'
-copyright = u'2010, Amos Joshua'
+copyright = u'2012, Amos Joshua'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
