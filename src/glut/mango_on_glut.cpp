ffc6457c7209414a4caf3b354eaa73f9826692d4
Philipp Möller <bootsarehax@googlemail.com>
2012-05-12 20:45:46 +0200

    Fix oops that prevented Mango_glut from being built

--- src/glut/mango_on_glut.cpp
+++ src/glut/mango_on_glut.cpp
@@ -19,7 +19,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
 */
-#include "mango_on_glut.h"
+#include <mango/glut/mango_on_glut.h>
 #include <iostream>
 
 namespace Mango{  
